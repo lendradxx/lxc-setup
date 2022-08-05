da5d7d128461e07cc021c2417734baf7019e5dfc
Lendra <lendradx@gmail.com>
2022-08-06 06:09:41 +0700

    Update archlinux.sh

--- archlinux.sh
+++ archlinux.sh
@@ -1,7 +1,7 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
+
 echo "[LOG]: Creating admin user..."
 useradd -mG wheel admin && echo -e "admin123\nadmin123" | passwd admin
-echo -e "%wheel ALL=(ALL:ALL) NOPASSWD: ALL" >>/etc/sudoers
 echo "[LOG]: Creating temporary mirror..."
 echo -e "## Worldwide\nServer = http://mirror.rackspace.com/archlinux/\$repo/os/\$arch\nServer = https://mirror.rackspace.com/archlinux/\$repo/os/\$arch" >/etc/pacman.d/mirrorlist
 echo "[LOG]: Updating keyring..."
@@ -12,9 +12,10 @@ pacman-key --refresh-keys
 pacman -Sy archlinux-keyring --noconfirm
 echo -e "\n"
 echo "[LOG]: Updating arch linux and installing base-devel"
-pacman -Syu --noconfirm && pacman -S base-devel git wget curl --needed --noconfirm
+pacman -Syu --noconfirm && pacman -S base-devel git wget curl sudo --needed --noconfirm
 echo -e "\n"
 echo "[LOG]: Installing ssh if needed..."
+echo -e "%wheel ALL=(ALL:ALL) NOPASSWD: ALL" >>/etc/sudoers
 pacman -S openssh --needed && systemctl enable --now sshd
 echo "[LOG]: Downloading yay..."
 wget https://github.com/Jguer/yay/releases/download/v11.2.0/yay_11.2.0_x86_64.tar.gz
