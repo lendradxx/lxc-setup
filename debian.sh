0fced6ed9e82ea7217be103751f8c1c831e206eb
Lendra <lendradx@gmail.com>
2022-10-09 13:55:04 +0700

    change

--- debian.sh
+++ debian.sh
@@ -5,7 +5,7 @@ PASSWORD="admin123"
 
 echo "[LOG]: Creating ssh user..."
 groupadd wheel
-useradd -mG wheel $USERNAME && echo -e "$PASSWORD\n$PASSWORD" | passwd $USERNAME
+useradd -mG wheel "$USERNAME" -s $(which bash 2>/dev/null) && echo -e "$PASSWORD\n$PASSWORD" | passwd $USERNAME
 echo "[LOG]: Updating system..."
 apt update && apt upgrade -y && apt install sudo openssh-server curl -y
 echo "[LOG]: Installing ssh if needed..."
