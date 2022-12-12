aaf47dccbca4134e38428703d1f6547defb20369
Lendra <lendradx@gmail.com>
2022-12-12 21:02:21 +0700

    Update

--- setup.sh
+++ setup.sh
@@ -115,9 +115,7 @@ function ArchInstall() {
 function UbuntuInstall() {
     echo "[LOG]: Creating backup for local repo..."
     echo -e "deb http://kartolo.sby.datautama.net.id/ubuntu/ ${UBUNTU_CODENAME} main restricted universe multiverse\ndeb http://kartolo.sby.datautama.net.id/ubuntu/ ${UBUNTU_CODENAME}-updates main restricted universe multiverse\ndeb http://kartolo.sby.datautama.net.id/ubuntu/ ${UBUNTU_CODENAME}-security main restricted universe multiverse" >/etc/apt/sources.list
-    echo "[LOG]: Creating ssh user..."
-    groupadd wheel
-    useradd -mG wheel $USERNAME -s $(which bash 2>/dev/null) && echo -e "$PASSWORD\n$PASSWORD" | passwd $USERNAME
+    CreateLoginUser
     echo "[LOG]: Updating system..."
     apt update && apt upgrade -y && apt install sudo openssh-server curl -y
     echo "[LOG]: Installing ssh if needed..."
